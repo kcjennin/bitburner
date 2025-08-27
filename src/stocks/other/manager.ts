925706ee81b034e9e438be91e274c4aee7729dad
Kyle Jennings <57046591+kcjennin@users.noreply.github.com>
2025-08-27 15:12:22 -0700

    Create manager.ts

---
+++ src/stocks/other/manager.ts
@@ -0,0 +1,31 @@
+import { StockMaster } from './StockMaster'
+
+function initializeHUD() {
+  const d = eval('document') as Document;
+  let htmlDisplay = d.getElementById('stock-display-1');
+  if (htmlDisplay !== null) return htmlDisplay;
+
+  const customElements = d.getElementById('overview-extra-hook-0')?.parentElement?.parentElement;
+  if (!customElements) throw 'Failed to get custom elements.'
+  const stockValueTracker = customElements.cloneNode(true) as HTMLElement;
+
+  stockValueTracker.querySelectorAll('p > p').forEach(el => el.parentElement?.removeChild(el))
+  stockValueTracker.querySelectorAll('p').forEach((el, i) => el.id = `stock-display-${i}`)
+  htmlDisplay = stockValueTracker.querySelector('#stock-display-1') as HTMLElement;
+  stockValueTracker.querySelectorAll('p')[0].innerText = 'Stock';
+  htmlDisplay.innerText = '$0.000 '
+  customElements.parentElement?.insertBefore(stockValueTracker, customElements.parentElement?.childNodes[2]);
+
+  return htmlDisplay;
+}
+
+export async function main(ns: NS) {
+  ns.disableLog('getServerMoneyAvailable');
+  ns.clearLog();
+
+  const hudElement = initializeHUD();
+  ns.atExit(() => hudElement.parentElement?.parentElement?.parentElement?.removeChild(hudElement.parentElement?.parentElement))
+  
+  const sm = new StockMaster(ns, hudElement);
+  await sm.smRun();
+}
